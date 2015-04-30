e98239167fc29c533c16411b1af7102cc95b86f2
Soumyajit Sarkar <soumyarokz@gmail.com>
2015-04-30 00:09:19 -0500

    Create buyItem.java

---
+++ buyItem.java
@@ -0,0 +1,58 @@
+package item;
+public class Item {
+    String itemid;
+    String itemimage;
+    String itemprice;
+    String itemname;
+    String itemtype;
+    String itemingredient;
+
+    public String getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(String itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getItemimage() {
+        return itemimage;
+    }
+
+    public void setItemimage(String itemimage) {
+        this.itemimage = itemimage;
+    }
+
+    public String getItemprice() {
+        return itemprice;
+    }
+
+    public void setItemprice(String itemprice) {
+        this.itemprice = itemprice;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public String getItemtype() {
+        return itemtype;
+    }
+
+    public void setItemtype(String itemtype) {
+        this.itemtype = itemtype;
+    }
+
+    public String getItemingredient() {
+        return itemingredient;
+    }
+
+    public void setItemingredient(String itemingredient) {
+        this.itemingredient = itemingredient;
+    }
+    
+}
